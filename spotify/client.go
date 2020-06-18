9edb6e77ed7614fac35385fd150a638f11ef5e5f
Mike Webster <mwebster@wyzant.com>
2020-06-18 14:01:03 -0500

    get top tracks

--- spotify/client.go
+++ spotify/client.go
@@ -1,8 +1,49 @@
 package spotify
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-func GetTopTracks(ctx context.Context) (*Tracks, error) {
-	// TODO
-	return nil, nil
+func getTopTracks(ctx context.Context) (Tracks, error) {
+	token := ctx.Value("access_token")
+	if token == nil {
+		return nil, errors.New("no access token provided")
+	}
+	url := "https://api.spotify.com/v1/me/top/tracks"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprint("Bearer ", token))
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("non-200 response")
+	}
+
+	type tempResp struct {
+		Items Tracks `json:"items"`
+	}
+
+	var ret tempResp
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, &ret)
+	if err != nil {
+		return nil, err
+	}
+	return ret.Items, nil
 }
