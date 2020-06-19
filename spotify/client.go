c7f440e5b33e69ba148e182be6c8a605c5b46858
Mike Webster <mwebster@wyzant.com>
2020-06-19 15:30:13 -0500

    add functionality to get top artists

--- spotify/client.go
+++ spotify/client.go
@@ -55,3 +55,54 @@ func getTopTracks(ctx context.Context) (Tracks, error) {
 	}
 	return ret.Items, nil
 }
+
+// decided not to make this one method that accepted the resource
+// but went against it to simplify use and avoid passing back
+// an interface.
+
+func getTopArtists(ctx context.Context) (*Artists, error) {
+	token := ctx.Value("access_token")
+	if token == nil {
+		return nil, errors.New("no access token provided")
+	}
+	tr := ctx.Value("time_range")
+	strRange := ""
+	if tr != nil {
+		strRange = tr.(string)
+	}
+	url := "https://api.spotify.com/v1/me/top/artists?limit=25"
+	if len(strRange) > 0 {
+		url += fmt.Sprint("&time_range=", strRange)
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprint("Bearer ", token))
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("non-200 response")
+	}
+
+	type tempResp struct {
+		Items Artists `json:"items"`
+	}
+
+	var ret tempResp
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, &ret)
+	if err != nil {
+		return nil, err
+	}
+	return &ret.Items, nil
+}
