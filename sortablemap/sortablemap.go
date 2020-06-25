9d4ea7bca1bf03b75fe46e205ab2753c5b64a83d
Mike Webster <mwebster@wyzant.com>
2020-06-24 20:36:52 -0500

    rename

--- sortablemap/sortablemap.go
+++ sortablemap/sortablemap.go
@@ -1,42 +1,44 @@
 package sortablemap
 
-type SortableMapItem struct {
 // Item represents an element in a SortableMap
+type Item struct {
 	Key   string
 	Value int32
 }
-type SortableMap []SortableMapItem
 
-func (p SortableMap) Len() int           { return len(p) }
-func (p SortableMap) Less(i, j int) bool { return p[i].Value < p[j].Value }
-func (p SortableMap) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p SortableMap) Contains(key string) int {
-	for i, ii := range p {
 // Map is a sortable map
+type Map []Item
 
+func (m Map) Len() int           { return len(m) }
+func (m Map) Less(i, j int) bool { return m[i].Value < m[j].Value }
+func (m Map) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+
+// Contains returns the index of the key if it exists in the map. If no match
+// is found -1 is returned.
+func (m Map) Contains(key string) int {
+	for i, ii := range m {
 		if ii.Key == key {
 			return i
 		}
 	}
 	return -1
 }
-func (p SortableMap) ToMap() map[string]int32 {
 
 // ToMap will return a basic map version of the SortableMap
+func (m Map) ToMap() map[string]int32 {
 	ret := map[string]int32{}
-	for _, i := range p {
+	for _, i := range m {
 		ret[i.Key] = i.Value
 	}
 	return ret
 }
 
-
-func GetSortableMap(m map[string]int) SortableMap {
-	ret := SortableMap{}
 // GetSortableMap takes a map and returns a map that can be
 // sorted.
+func GetSortableMap(m map[string]int) Map {
+	ret := Map{}
 	for k, v := range m {
-		ret = append(ret, SortableMapItem{Key: k, Value: int32(v)})
+		ret = append(ret, Item{Key: k, Value: int32(v)})
 	}
 	return ret
 }
