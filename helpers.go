48f2ea59c103249fb0374ded4c5111742d54b5ec
Mike Webster <mwebster@wyzant.com>
2020-06-27 16:52:03 -0500

    assign return url

--- helpers.go
+++ helpers.go
@@ -32,6 +32,7 @@ func parseEnvironmentVariables(ctx context.Context) (context.Context, error) {
 	if len(host) < 1 {
 		return nil, errors.New("no host provided")
 	}
+	returnURL = fmt.Sprint("https://", host, "/spotify/oauth")
 	// TODO: Do we need this in the context? or just set for the main package?
 	// consider: the main goal here is to be able to verify everything is working
 	// on app start using the context returned from this method.
