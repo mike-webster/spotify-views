6bae8ba2f6be607b901f9fc970e9ae7c7af37dbc
Mike Webster <mwebster@wyzant.com>
2020-06-27 14:11:13 -0500

    script to add tables

---
+++ data/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    spotify_id VARCHAR(200) NOT NULL,
+    UNIQUE KEY (spotify_id)
+);
+
+CREATE TABLE IF NOT EXISTS tokens (
+    spotify_id VARCHAR(200) NOT NULL PRIMARY KEY,
+    refresh VARCHAR(200) NOT NULL,
+    UNIQUE KEY (refresh)
+);
