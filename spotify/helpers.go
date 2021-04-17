df73436a43db325137be556dd24a4d181a18d485
Mike Webster <email@mikewebster.tech>
2021-04-17 17:07:54 -0500

    added enum

--- spotify/helpers.go
+++ spotify/helpers.go
@@ -8,3 +8,24 @@ func getPairs(m map[string]int32) Pairs {
 
 	return ret
 }
+
+type TimeFrame int
+
+const (
+    TFShort TimeFrame = iota
+    TFMedium
+    TFLong
+)
+
+func (t TimeFrame) Value() string {
+	switch t {
+	case TFShort:
+		return "short_term"
+	case TFMedium:
+		return "medium_term"
+	case TFLong:
+		return "long_term"
+	default:
+		return ""
+	}
+}
