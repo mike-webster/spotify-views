079396e1c80f9faf8d888f1187800ffd4880eb0b
Mike Webster <email@mikewebster.tech>
2021-05-29 03:00:44 -0500

    fix coors

--- router/middleware.go
+++ router/middleware.go
@@ -157,7 +157,11 @@ func parseLoggerValues(c *gin.Context) *logging.LoggerFields {
 }
 
 func CORSMiddleware(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	if os.Getenv("GO_ENV") != "production" {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	} else {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "spotify-views.com")
+	}
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
