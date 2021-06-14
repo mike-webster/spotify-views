3da86267892a9a8e38420069985154c0a23cf828
Mike Webster <emailmikewebster@gmail.com>
2021-06-13 20:06:43 -0500

    terrible idea, this is handled server side now

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,7 +3,6 @@ import Home from './Home';
 import ActionSelect from './ActionSelect';
 import WordCloud from './WordCloud';
 import Library from './Library';
-import SpotifyOAuth from './SpotifyOAuth';
 import {Route, Switch } from 'react-router-dom';
 
 export default function Router() {
@@ -14,7 +13,6 @@ export default function Router() {
         <Route exact path="/discover" component={ActionSelect} />
         <Route exact path="/wordcloud" component={WordCloud} />
         <Route exact path="/library" component={Library} />
-        <Route exact path="/spotify/oauth" component={SpotifyOAuth} />
       </Switch>
     </main>
   );
