33d2dbc2292afb2ffb92363aad548685ae569a99
Mike Webster <emailmikewebster@gmail.com>
2022-08-14 02:01:49 -0500

    fix layout

--- client/src/Home.js
+++ client/src/Home.js
@@ -44,21 +44,20 @@ export default class Home extends React.Component {
         return (
             <Layout >
                 <div className="container">
-                    <div className="home-banner">
-                        <div className="blurry-back-white">
+                    <div className="panel blurry-back-white">
+                        <div className="large-pad-bottom">
                             <h1 className="full-width center-text large-pad-top">Spotify Views</h1>
                             <p className="full-width center-text">Find More Of What You Love</p>
                         </div>
-                        <div className="fix"></div>
-                        <div className="blurry-back-white large-pad-v large-marg-top">
+                        <div className="large-pad-v large-marg-top">
                             <Button path="/login?redirectUrl=?authed" text="LOG IN WITH SPOTIFY" css="btn half-width center-text" />
                         </div>
+                        <div className="med-pad-v large-pad-h">
+                            Using Spotify Views, you can take a dive into your music taste to discover more about the music you love! Log in with your existing
+                            Spotify account and learn about your top tracks, artists and genres, get recommendations for new artists, and more!
+                        </div>
+                        <CookieBanner />
                     </div>
-                    <p className="white-back small-marg med-pad-v large-pad-h">
-                        Using Spotify Views, you can take a dive into your music taste to discover more about the music you love! Log in with your existing
-                        Spotify account and learn about your top tracks, artists and genres, get recommendations for new artists, and more!
-                    </p>
-                    <CookieBanner />
                 </div>
             </Layout>
         );
