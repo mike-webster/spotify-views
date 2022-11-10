57acf37f9a3a82307fa8bd8695349a0fb45ddc2d
Mike Webster <emailmikewebster@gmail.com>
2022-11-10 00:28:50 -0600

    remove banner

--- client/src/Home.js
+++ client/src/Home.js
@@ -50,8 +50,8 @@ export default class Home extends React.Component {
                             <p className="full-width center-text">Find More Of What You Love</p>
                         </div>
                         <div className="large-pad-v large-marg-top">
-                            {/* <Button path="/login?redirectUrl=?authed" text="LOG IN WITH SPOTIFY" css="btn half-width center-text" /> */}
-                            <h3 className="large-pad-top large-pad-bottom" style={{width: "80%", margin: "auto", backgroundColor:  "rgba(29, 185, 84, 0.75)", textAlign: "center", borderRadius: "25px"}}>Apologies for the inconvenience! We are down for the short term while we make some upgrades to our service. Please check back later!</h3>
+                            <Button path="/login?redirectUrl=?authed" text="LOG IN WITH SPOTIFY" css="btn half-width center-text" />
+                            {/* <h3 className="large-pad-top large-pad-bottom" style={{width: "80%", margin: "auto", backgroundColor:  "rgba(29, 185, 84, 0.75)", textAlign: "center", borderRadius: "25px"}}>Apologies for the inconvenience! We are down for the short term while we make some upgrades to our service. Please check back later!</h3> */}
                         </div>
                         <div className="med-pad-v large-pad-h">
                             Using Spotify Views, you can take a dive into your music taste to discover more about the music you love! Log in with your existing
