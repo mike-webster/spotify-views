ce52c8141d9755eb7aec6b9b125cf35404a924b7
Mike Webster <emailmikewebster@gmail.com>
2022-11-14 22:20:29 -0600

    small fixes

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,7 +6,8 @@ export default class Footer extends React.Component {
         return (
             <div className="footer">
                 <a href="mailto:spotify-views@webstercode.com">Contact</a>
-                <div className="blank"></div>
+                {/* <div className="blank"></div> */}
+                <a href="https://www.mikewebster.io">Mike Webster</a>
                 <a href="https://www.spotify.com">Spotify</a>
             </div>
         );
